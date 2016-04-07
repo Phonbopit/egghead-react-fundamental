e74778b1425f60345cb3b7f945fd16924c19e7ad
Chai Phonbopit <phonbopit@gmail.com>
2016-04-08 02:18:10 +0700

    add readme

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      txt: 'this is the state txt',
+      cat: 51
+    }
+  }
+
+  update(e) {
+    this.setState({
+      txt: e.target.value
+    })
+  }
+
+  onChange(e) {
+    this.setState({
+      txt: e.target.value
+    })
+  }
+
   render() {
     return (
       <div>
         <h1>Hello World</h1>
-        <p>This is lorem</p>
+        <p>Title : {this.state.txt}</p>
+        <p>Cat : {this.state.cat}</p>
+        <input type="text" value={this.state.txt} onChange={this.onChange.bind(this)} />
       </div>
     );
   }
---
+++ README.md
@@ -0,0 +1,4 @@
+es6-react
+---
+
+Learn from [React Fundamentals](https://egghead.io/series/react-fundamentals)
